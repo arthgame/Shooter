94e44899218b1fca5941ed7e088ce892e4bcd967
Fisent <lukzieniewicz@gmail.com>
2016-11-04 12:54:29 +0100

    Dodany interferes IMovement

---
+++ Assets/Scripts/Interfaces/IMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IMovement {
+
+	Vector2 Direction{ set; get;}
+
+	bool IsMoving{ get;}
+
+	float CurrentVelocity{ get;}
+
+	float Speed{ set; get;}
+}
